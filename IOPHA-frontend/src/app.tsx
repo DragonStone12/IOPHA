2c5e6bf9a366216481574800290b7aa44701a9d7
Jamaal Long <jamaal.long@example.com>
2026-06-28 12:46:02 -0500

    fix: restore Profiler, performance metrics, and error boundaries in app.tsx

--- IOPHA-frontend/src/app.tsx
+++ IOPHA-frontend/src/app.tsx
@@ -1,21 +1,39 @@
 import "./index.css";
 import { QueryProvider } from "./providers/QueryProvider";
 import { LandingPage } from "./components/LandingPage/LandingPage";
+import { AppErrorBoundary } from "./components/AppErrorBoundary";
+import { UserDisplay } from "./components/UserDisplay";
+import { onRenderCallback, logPagePerformanceMetrics } from "./utils/performance";
+import { appRenderDebug } from "./utils/logger";
 import ReactDOM from "react-dom/client";
-import React from "react";
+import React, { Profiler } from "react";
 
-function App() {
-  return (
-    <QueryProvider>
-      <LandingPage />
-    </QueryProvider>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const App = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AppErrorBoundary boundaryName="Root">
+      <h1>IOPHA - Interactive Obesity Prevention Health Assistant</h1>
+
+      <AppErrorBoundary boundaryName="UserDisplay">
+        <UserDisplay />
+      </AppErrorBoundary>
+
+      <AppErrorBoundary boundaryName="LandingPage">
+        <LandingPage />
+      </AppErrorBoundary>
+    </AppErrorBoundary>
+  </React.StrictMode>
+);
+
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
+
+appRenderDebug("Mounting IOPHA application root");
+logPagePerformanceMetrics();
+
+root.render(
+  <Profiler id="IOPHA-App" onRender={onRenderCallback}>
+    <QueryProvider>{App}</QueryProvider>
+  </Profiler>,
 );
 
 export default App;
